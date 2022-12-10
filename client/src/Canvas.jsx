15ca315e0d5b3fb4f2bafef547f32bcada2d1a41
HectorRubio2 <hector.1701710051@ucaldas.edu.co>
2022-12-09 22:30:14 -0500

    canvas con imagnes

--- client/src/Canvas.jsx
+++ client/src/Canvas.jsx
@@ -1,39 +1,77 @@
+import React, { useRef, useEffect } from "react";
+import canibal from "./assets/canibal.jpg";
+import misionero from './assets/misionero.png';
 
-import React, { useRef, useEffect } from 'react'
+const Canvas = (props) => {
+  const canvasRef = useRef(null);
 
-const Canvas = props => {
-  
-  const canvasRef = useRef(null)
-  
-  const draw = (ctx, frameCount) => {
-    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height)
-    ctx.fillStyle = '#000000'
-    ctx.beginPath()
-    ctx.arc(50, 100, 20*Math.sin(frameCount*0.05)**2, 0, 2*Math.PI)
-    ctx.fill()
-  }
-  
+  const misionero1 = (ctx, frameCount) => {
+    var imageObj1 = new Image();
+    imageObj1.src = misionero;
+    imageObj1.onload = function () {
+      ctx.drawImage(imageObj1, 30, 30, 30, 30);
+    };
+  };
+  const misionero2 = (ctx, frameCount) => {
+    var imageObj1 = new Image();
+    imageObj1.src = misionero;
+    imageObj1.onload = function () {
+      ctx.drawImage(imageObj1, 30, 60, 30, 30);
+    };
+  };
+  const misionero3 = (ctx, frameCount) => {
+    var imageObj1 = new Image();
+    imageObj1.src = misionero;
+    imageObj1.onload = function () {
+      ctx.drawImage(imageObj1, 30,90 , 30, 30);
+    };
+  };
+  const canibal1 = (ctx, frameCount) => {
+    var imageObj1 = new Image();
+    imageObj1.src = canibal;
+    imageObj1.onload = function () {
+      ctx.drawImage(imageObj1, 0, 30, 30, 30);
+    };
+  };
+  const canibal2 = (ctx, frameCount) => {
+    var imageObj1 = new Image();
+    imageObj1.src = canibal;
+    imageObj1.onload = function () {
+      ctx.drawImage(imageObj1, 0, 60, 30, 30);
+    };
+  };
+  const canibal3 = (ctx, frameCount) => {
+    var imageObj1 = new Image();
+    imageObj1.src = canibal;
+    imageObj1.onload = function () {
+      ctx.drawImage(imageObj1, 0,90 , 30, 30);
+    };
+  };
   useEffect(() => {
-    
-    const canvas = canvasRef.current
-    const context = canvas.getContext('2d')
-    let frameCount = 0
-    let animationFrameId
-    
+    const canvas = canvasRef.current;
+    const context = canvas.getContext("2d");
+    let frameCount = 1;
+    let animationFrameId;
+
     //Our draw came here
     const render = () => {
-      frameCount++
-      draw(context, frameCount)
-      animationFrameId = window.requestAnimationFrame(render)
-    }
-    render()
-    
-    return () => {
-      window.cancelAnimationFrame(animationFrameId)
-    }
-  }, [draw])
-  
-  return <canvas ref={canvasRef} {...props} className="canvas"/>
-}
+      frameCount++;
+      canibal1(context, frameCount);
+      canibal2(context, frameCount);
+      canibal3(context, frameCount);
+      misionero1(context, frameCount);
+      misionero2(context, frameCount);
+      misionero3(context, frameCount);
+      animationFrameId = window.requestAnimationFrame(render);
+    };
+    render();
 
-export default Canvas
+    return () => {
+      window.cancelAnimationFrame(animationFrameId);
+    };
+  }, [canibal1, canibal2, canibal3,misionero1,misionero2,misionero3]);
+
+  return <canvas ref={canvasRef} {...props} className="canvas" />;
+};
+
+export default Canvas;
